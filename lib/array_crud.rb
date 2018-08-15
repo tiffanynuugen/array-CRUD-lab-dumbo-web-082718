e24b53a37a38c857d2c77dae1546507abf5ff955
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 20:14:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,8 +14,8 @@ def add_element_to_start_of_array(fruits, clementine)
   fruits.unshift(clementine)
 end
 
-def remove_element_from_end_of_array(array)
-
+def remove_element_from_end_of_array(fruits)
+  fruits.pop
 end
 
 def remove_element_from_start_of_array(array)
