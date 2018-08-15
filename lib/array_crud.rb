0b389f98b73615aa95bead906d2d3a99ff331148
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 20:24:27 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  fruits = ["avocado", "papaya", "guava", "strawberry"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(fruits, orange)
+  fruits.push(orange)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(fruits, clementine)
+  fruits.unshift(clementine)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(fruits)
+  fruits.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(fruits)
+  fruits.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(fruits, one)
+  fruits[one]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(fruits)
+  fruits[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(fruits)
+  item = fruits.count
+  fruits[item - 1]
 end
