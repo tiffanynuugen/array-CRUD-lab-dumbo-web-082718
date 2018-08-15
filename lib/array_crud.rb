32564787794827b1929071a7b8e350b684cb6743
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 20:17:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_start_of_array(fruits)
   fruits.shift
 end
 
-def retrieve_element_from_index(fruits, 1)
-  fruits[1]
+def retrieve_element_from_index(fruits, 2)
+  fruits[2]
 end
 
 def retrieve_first_element_from_array(array)
