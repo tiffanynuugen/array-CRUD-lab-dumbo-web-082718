cf07bdd4cf2836e0feec30269d48fea851b9b626
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 19:58:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  fruits.push("orange")
+  .push(fruits, "orange")
 end
 
 def add_element_to_start_of_array(array, element)
