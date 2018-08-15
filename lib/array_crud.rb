8581b2460bfcd74e443dac1655742673392da336
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 20:03:59 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  fruits = ["avocado", "papaya", "guava", "strawberry"]
+  ["avocado", "papaya", "guava", "strawberry"]
 end
 
 def add_element_to_end_of_array(array, element)
-  fruits << "orange"
+  array << "orange"
 end
 
 def add_element_to_start_of_array(array, element)
